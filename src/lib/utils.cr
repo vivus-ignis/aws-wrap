a0d6f4697be859f6531ca0ba362bf33d3d5c8e8b
Vivus Ignis <ignis@posteo.de>
2019-02-15 08:51:20 +0100

    experimenting with pipe detection

--- src/lib/utils.cr
+++ src/lib/utils.cr
@@ -15,8 +15,20 @@ module Utils
     STDERR.puts "[#{Process.pid}] DEBUG // #{msg}" if is_debug_on?
   end
 
-  def got_pipe?(fd)
-    ! fd.tty?
+  def got_pipe?(filedes : IO::FileDescriptor)
+    Utils.dputs("#{filedes.fd}")
+    dev_file = case filedes.fd
+               when 0
+                 "/dev/stdin"
+               when 1
+                 "/dev/stdout"
+               when 2
+                 "/dev/stderr"
+               else
+                 return ! filedes.tty? # fallback method to test
+               end
+
+    File.info(dev_file).type.pipe?
   end
 
 end
